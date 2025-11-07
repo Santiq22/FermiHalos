229d746b8099248333146826dab0dec6d8118345
Santiq22 <luciopoto@gmail.com>
2025-11-07 16:02:07 -0300

    Started to add docstrings to the rar_class.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,9 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = []
